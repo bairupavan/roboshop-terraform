74f0d98fd0d3149b19608f53eb93c4e47397b32a
pavanbairu <pavan.bairu1@gmail.com>
2023-10-10 19:44:24 +0530

    provisioner to connect to the host and execute the commands

--- main.tf
+++ main.tf
@@ -9,11 +9,32 @@ resource "aws_instance" "instance" {
   }
 }
 
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance, aws_route53_record.dns]
+
+  for_each = var.components
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = aws_instance.instance[each.value["name"]].private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "rm -rf roboshop-shell-script",
+      "git clone https://github.com/pavanbairu/roboshop-shell-script.git ",
+      "cd roboshop-shell-script",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
+}
 resource "aws_route53_record" "dns" {
   for_each = var.components
   zone_id = "Z08846229MEF59DJAKAS"
   name    = "${each.value["name"]}-dev.pavanbairu.tech"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.instance[each.value["name"]].public_ip]
+  records = [aws_instance.instance[each.value["name"]].private_ip]
 }
